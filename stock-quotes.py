44f81b370b55d101c3c008daef651e0db7e40f1b
Paul Murphy <pdm_tech@yahoo.com>
2022-02-09 19:39:52 -0500

    2/9/22

--- stock-quotes.py
+++ stock-quotes.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import openpyxl
 from openpyxl import Workbook
 
@@ -21,12 +23,21 @@ print(f"max rows in sheet:{number_rows}")
 #control web browser with web driver
 #driver = webdriver.Chrome(".\chromedriver.exe")
 #driver = webdriver.chrome()
-driver = webdriver.Firefox(".\geckodriver.exe")
-driver = webdriver.Firefox
+
+driver = Service(r'C:\\Users\\user\\Downloads\\python\\Selenium\\selenium-tests\\geckodriver.exe')
+#driver = webdriver.Firefox(executable_path=r'C:\Users\user\Downloads\python\Selenium\selenium-tests\geckodriver.exe')
 driver.get("https://finance.yahoo.com/")
+quotelookup = driver.find_element(By.CSS_SELECTOR,'#darla_csc_holder').send_keys(ticker)
+quotelookup = driver.find_element(By.CSS_SELECTOR,'#darla_csc_holder').send_keys(Keys.ENTER)
+#quotelookup = driver.find_element_by_css_selector('#darla_csc_holder').send_keys(ticker) #  #input.D\(ib\)
+#quotelookup = driver.find_element_by_css_selector('#darla_csc_holder').send_keys(Keys.ENTER)  ##darla_csc_holder #input.D\(ib\)
+
 
 #loop through all rows, skipping title row 1
 for symbol in range(2,number_rows+1):
     ticker = sheet['A'+ str(symbol)].value
     print(f"ticker={ticker}")
+    #put current ticker into "quote lookup" box
+    quotelookup = driver.find_element_by_css_selector('input.D\(ib\)').send_keys(ticker)
+    quotelookup = driver.find_element_by_css_selector('input.D\(ib\)').send_keys(Keys.ENTER)
 
